53bcf1570a011d332827495314e39dafe1cb7e74
John Menke <jmjava@gmail.com>
2019-04-28 13:55:13 -0400

    pointer vs value

--- Chapter01/composite/composite_test.go
+++ Chapter01/composite/composite_test.go
@@ -9,13 +9,20 @@ func TestComposite(t *testing.T){
 	var leaf1 = Leaflet{Name: "leaf 1"}
 	var leaf2 = Leaflet{Name: "leaf 2"}
 
-	var branch2 = Branch{Name: "branch 2"}
+	//NOTE first way can pass without pointer
+	//NOTE second way of creating branch with pointer
+	//NOTE third way of creating branch with new (use pointer to pass)
+
+	//var branch2 = Branch{Name: "branch 2"}  // can pass without pointer deref
+	//var branch2 = &Branch{Name: "branch 2"} // need pointer deref
+	var branch2 = new(Branch) // need pointer deref
+	branch2.Name = "branch 2"
 
 	branch.Add(leaf1)
 	branch.Add(leaf2)
-	branch.AddBranch(branch2)
+	//branch.AddBranch(branch2)
+	branch.AddBranch(*branch2)
 
 	branch.Perform()
-
 
 }
